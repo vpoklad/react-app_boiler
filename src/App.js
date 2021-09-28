fd35f0ed35244eb36026d2403acd3dd38ca77a71
Viktor Poklad <78758930+vpoklad@users.noreply.github.com>
2021-09-28 20:33:14 +0300

    Initial commit
    initial
    
    prettier settings
    
    trainee
    
    card
    
    добавляет статистику
    
    добавляет список друзей
    
    добавляет список транзакций
    
    деплой
    
    добавляет проптайп
    
    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import './App.css';
+
+import { Component } from 'react/cjs/react.production.min';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
